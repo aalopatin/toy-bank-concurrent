1086cbef05548bdec7f842b650df703376ade7be
Alexander Lopatin <lopatin-disk@yandex.ru>
2021-07-08 19:08:49 +0300

    small changes

--- src/ru/aal/FrontEndSystem.java
+++ src/ru/aal/FrontEndSystem.java
@@ -1,14 +1,17 @@
 package ru.aal;
 
-import java.nio.file.Path;
-import java.util.ArrayDeque;
-import java.util.NoSuchElementException;
-import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class FrontEndSystem {
-    private BlockingQueue<Request> requests = new ArrayBlockingQueue<>(2);
+    //Выбрал BlockingQueue потому, что этот интрефейс позволяет задать максимальный размер очереди,
+    //что соответсвует условию задачи, где требуется ограничить одновременное количество заявок
+    //во фронде 2мя.
+    //BlockingQueue автоматически переходит в ожидание, когда идет попытка положить n+1 элемент и когда
+    //идет попытка получить элемент из пустой очереди.
+    //Тем самым это нам позволяет обойтись без Lock механизмов для организации многопоточности.
+    private BlockingQueue<Request> requests = new LinkedBlockingQueue<>(2);
     private Request dummy;
 
     public FrontEndSystem(Request dummy) {
@@ -19,7 +22,8 @@ public class FrontEndSystem {
         try {
             requests.put(request);
             if(dummy != request) {
-                System.out.printf("Клиент(%s): %s отправлена в банк\n", request.getClientThreadName(), request);
+                System.out.printf("Клиент(%s): %s отправлена в банк\n",
+                        request.getClientThreadName(), request);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -30,12 +34,12 @@ public class FrontEndSystem {
         try {
             Request request = requests.take();
             if(dummy != request) {
-                System.out.printf("Обработчик (%s): получена заявка на обработку по клиенту - %s \n", Thread.currentThread().getName(), request.getClientThreadName());
+                System.out.printf("Обработчик (%s): получена заявка на обработку по клиенту - %s \n",
+                        Thread.currentThread().getName(), request.getClientThreadName());
             }
             return request;
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            return null;
         }
-        return null;
     }
 }
